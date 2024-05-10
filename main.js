164b0fc3fccafa01ec7bd1b1a32ea0c0068cb708
tonmoy-dev <rxdevs100@gmail.com>
2024-05-10 12:50:41 +0600

    refactor: Update main.js file by adding tailwindcss classes

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from "./counter.js";
+import javascriptLogo from "./javascript.svg";
+import "./main.css";
+import "./style.css";
+import viteLogo from "/vite.svg";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <div class="flex items-center justify-center">
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="${viteLogo}" class="logo text-3xl" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${javascriptLogo}" class="logo vanilla text-3xl" alt="JavaScript logo" />
+      </a>
+    </div>
+    <h1 class="text-4xl underline">Hello Vite!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Start with editing main.js file
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
